567b56a2c396bab517e1442ef6b082ecd6a4be7f
Ivan Razzhivin <underwit@altlinux.org>
2019-04-05 10:48:56 +0300

    refactoring

--- extract.py
+++ extract.py
@@ -1,68 +1,15 @@
 import argparse
 import os
+import datetime
 import rpm
 import psycopg2
-import datetime
+import mapper
 
 from utils import changelog_to_text, cvt, cvt_ts
 
 
 def insert_package(conn, hdr):
-    map_package = {
-        'sha1header': cvt(hdr[rpm.RPMDBI_SHA1HEADER]),
-        'name': cvt(hdr[rpm.RPMTAG_NAME]),
-        'arch': cvt(hdr[rpm.RPMTAG_ARCH]),
-        'version': cvt(hdr[rpm.RPMTAG_VERSION]),
-        'release': cvt(hdr[rpm.RPMTAG_RELEASE]),
-        'epoch': rpm.RPMTAG_EPOCH,
-        'serial_': rpm.RPMTAG_SERIAL,
-        'summary': cvt(hdr[rpm.RPMTAG_SUMMARY]),
-        'description': cvt(hdr[rpm.RPMTAG_DESCRIPTION]),
-        'changelog': changelog_to_text(
-            hdr[rpm.RPMTAG_CHANGELOGTIME],
-            hdr[rpm.RPMTAG_CHANGELOGNAME],
-            hdr[rpm.RPMTAG_CHANGELOGTEXT]),
-        'buildtime': hdr[rpm.RPMTAG_BUILDTIME],
-        'buildhost': cvt(hdr[rpm.RPMTAG_BUILDHOST]),
-        'size': hdr[rpm.RPMTAG_SIZE],
-        'distribution': cvt(hdr[rpm.RPMTAG_DISTRIBUTION]),
-        'vendor': cvt(hdr[rpm.RPMTAG_VENDOR]),
-        'gif': hdr[rpm.RPMTAG_GIF],
-        'xpm': hdr[rpm.RPMTAG_XPM],
-        'license': cvt(hdr[rpm.RPMTAG_LICENSE]),
-        'group_': cvt(hdr[rpm.RPMTAG_GROUP]),
-        'source': cvt(hdr[rpm.RPMTAG_SOURCE]),
-        'patch': cvt(hdr[rpm.RPMTAG_PATCH]),
-        'url': cvt(hdr[rpm.RPMTAG_URL]),
-        'os': cvt(hdr[rpm.RPMTAG_OS]),
-        'prein': cvt(hdr[rpm.RPMTAG_PREIN]),
-        'postin': cvt(hdr[rpm.RPMTAG_POSTIN]),
-        'preun': cvt(hdr[rpm.RPMTAG_PREUN]),
-        'postun': cvt(hdr[rpm.RPMTAG_POSTUN]),
-        'icon': hdr[rpm.RPMTAG_ICON],
-        'archivesize': rpm.RPMTAG_ARCHIVESIZE,
-        'rpmversion': cvt(hdr[rpm.RPMTAG_RPMVERSION]),
-        'preinprog': cvt(hdr[rpm.RPMTAG_PREINPROG]),
-        'postinprog': cvt(hdr[rpm.RPMTAG_POSTINPROG]),
-        'preunprog': cvt(hdr[rpm.RPMTAG_PREUNPROG]),
-        'postunprog': cvt(hdr[rpm.RPMTAG_POSTUNPROG]),
-        'buildarchs': cvt(hdr[rpm.RPMTAG_BUILDARCHS]),
-        'verifyscript': cvt(hdr[rpm.RPMTAG_VERIFYSCRIPT]),
-        'verifyscriptprog': cvt(hdr[rpm.RPMTAG_VERIFYSCRIPTPROG]),
-        'cookie': cvt(hdr[rpm.RPMTAG_COOKIE]),
-        'prefixes': cvt(hdr[rpm.RPMTAG_PREFIXES]),
-        'instprefixes': cvt(hdr[rpm.RPMTAG_INSTPREFIXES]),
-        'sourcepackage': bool(rpm.RPMTAG_SOURCEPACKAGE),
-        'optflags': cvt(hdr[rpm.RPMTAG_OPTFLAGS]),
-        'disturl': cvt(hdr[rpm.RPMTAG_DISTURL]),
-        'payloadformat': cvt(hdr[rpm.RPMTAG_PAYLOADFORMAT]),
-        'payloadcompressor': cvt(hdr[rpm.RPMTAG_PAYLOADCOMPRESSOR]),
-        'payloadflags': cvt(hdr[rpm.RPMTAG_PAYLOADFLAGS]),
-        'platform': cvt(hdr[rpm.RPMTAG_PLATFORM]),
-        'sourcepkgid': hdr[rpm.RPMTAG_SOURCEPKGID],
-        'disttag': cvt(hdr[rpm.RPMTAG_DISTTAG]),
-    }
-
+    map_package = mapper.get_package_map(hdr)
     sql = (
             'INSERT INTO Package ({0}) VALUES ({1})'
             ' ON CONFLICT DO NOTHING RETURNING id'
@@ -76,53 +23,20 @@ def insert_package(conn, hdr):
     package_id = cur.fetchone()
     if package_id:
         package_id = package_id[0]
-        map_files = {
-            'filename': cvt(hdr[rpm.RPMTAG_FILENAMES]),
-            'filesize': cvt(hdr[rpm.RPMTAG_FILESIZES]),
-            'filemode': cvt(hdr[rpm.RPMTAG_FILEMODES]),
-            'filerdev': cvt(hdr[rpm.RPMTAG_FILERDEVS]),
-            'filemtime': cvt_ts(hdr[rpm.RPMTAG_FILEMTIMES]),
-            'filemd5': cvt(hdr[rpm.RPMTAG_FILEMD5S]),
-            'filelinkto': cvt(hdr[rpm.RPMTAG_FILELINKTOS]),
-            'fileflag': cvt(hdr[rpm.RPMTAG_FILEFLAGS]),
-            'fileusername': cvt(hdr[rpm.RPMTAG_FILEUSERNAME]),
-            'filegroupname': cvt(hdr[rpm.RPMTAG_FILEGROUPNAME]),
-            'fileverifyflag': cvt(hdr[rpm.RPMTAG_FILEVERIFYFLAGS]),
-            'filedevice': cvt(hdr[rpm.RPMTAG_FILEDEVICES]),
-            'fileinode': cvt(hdr[rpm.RPMTAG_FILEINODES]),
-            'filelang': cvt(hdr[rpm.RPMTAG_FILELANGS]),
-            'fileclass': cvt(hdr[rpm.RPMTAG_FILECLASS]),
-            'dirindex': cvt(hdr[rpm.RPMTAG_DIRINDEXES]),
-            'basename': cvt(hdr[rpm.RPMTAG_BASENAMES]),
-        }
+
+        map_files = mapper.get_file_map(hdr)
         insert_list(cur, map_files, package_id, 'File')
 
-        map_require = {
-            'name': cvt(hdr[rpm.RPMTAG_REQUIRENAME]),
-            'version': cvt(hdr[rpm.RPMTAG_REQUIREVERSION]),
-            'flag': hdr[rpm.RPMTAG_REQUIREFLAGS],
-        }
+        map_require = mapper.get_require_map(hdr)
         insert_list(cur, map_require, package_id, 'Require')
 
-        map_conflict = {
-            'name': cvt(hdr[rpm.RPMTAG_CONFLICTNAME]),
-            'version': cvt(hdr[rpm.RPMTAG_CONFLICTVERSION]),
-            'flag': hdr[rpm.RPMTAG_CONFLICTFLAGS],
-        }
+        map_conflict = mapper.get_conflict_map(hdr)
         insert_list(cur, map_conflict, package_id, 'Conflict')
 
-        map_obsolete = {
-            'name': cvt(hdr[rpm.RPMTAG_OBSOLETENAME]),
-            'version': cvt(hdr[rpm.RPMTAG_OBSOLETEVERSION]),
-            'flag': hdr[rpm.RPMTAG_OBSOLETEFLAGS],
-        }
+        map_obsolete = mapper.get_obsolete_map(hdr)
         insert_list(cur, map_obsolete, package_id, 'Obsolete')
 
-        map_provide = {
-            'name': cvt(hdr[rpm.RPMTAG_PROVIDENAME]),
-            'version': cvt(hdr[rpm.RPMTAG_PROVIDEVERSION]),
-            'flag': hdr[rpm.RPMTAG_PROVIDEFLAGS],
-        }
+        map_provide = mapper.get_provide_map(hdr)
         insert_list(cur, map_provide, package_id, 'Provide')
 
     conn.commit()
@@ -161,9 +75,8 @@ def load(args):
     err_cnt = 0
     package_cnt = 0
     for package in packages:
-        print('Package: {0}'.format(package))
-        header = get_header(ts, package)
         try:
+            header = get_header(ts, package)
             insert_package(conn, header)
         except (Exception, psycopg2.DatabaseError) as error:
             print(error)
@@ -178,7 +91,6 @@ def load(args):
             break
         else:
             package_cnt += 1
-            print('Package successful loaded')
 
     if conn is not None:
         conn.close()
---
+++ mapper.py
@@ -0,0 +1,120 @@
+import rpm
+
+from utils import changelog_to_text, cvt, cvt_ts
+
+
+def get_package_map(hdr):
+    map_package = {
+        'sha1header': cvt(hdr[rpm.RPMDBI_SHA1HEADER]),
+        'name': cvt(hdr[rpm.RPMTAG_NAME]),
+        'arch': cvt(hdr[rpm.RPMTAG_ARCH]),
+        'version': cvt(hdr[rpm.RPMTAG_VERSION]),
+        'release': cvt(hdr[rpm.RPMTAG_RELEASE]),
+        'epoch': rpm.RPMTAG_EPOCH,
+        'serial_': rpm.RPMTAG_SERIAL,
+        'summary': cvt(hdr[rpm.RPMTAG_SUMMARY]),
+        'description': cvt(hdr[rpm.RPMTAG_DESCRIPTION]),
+        'changelog': changelog_to_text(
+            hdr[rpm.RPMTAG_CHANGELOGTIME],
+            hdr[rpm.RPMTAG_CHANGELOGNAME],
+            hdr[rpm.RPMTAG_CHANGELOGTEXT]),
+        'buildtime': hdr[rpm.RPMTAG_BUILDTIME],
+        'buildhost': cvt(hdr[rpm.RPMTAG_BUILDHOST]),
+        'size': hdr[rpm.RPMTAG_SIZE],
+        'distribution': cvt(hdr[rpm.RPMTAG_DISTRIBUTION]),
+        'vendor': cvt(hdr[rpm.RPMTAG_VENDOR]),
+        'gif': hdr[rpm.RPMTAG_GIF],
+        'xpm': hdr[rpm.RPMTAG_XPM],
+        'license': cvt(hdr[rpm.RPMTAG_LICENSE]),
+        'group_': cvt(hdr[rpm.RPMTAG_GROUP]),
+        'source': cvt(hdr[rpm.RPMTAG_SOURCE]),
+        'patch': cvt(hdr[rpm.RPMTAG_PATCH]),
+        'url': cvt(hdr[rpm.RPMTAG_URL]),
+        'os': cvt(hdr[rpm.RPMTAG_OS]),
+        'prein': cvt(hdr[rpm.RPMTAG_PREIN]),
+        'postin': cvt(hdr[rpm.RPMTAG_POSTIN]),
+        'preun': cvt(hdr[rpm.RPMTAG_PREUN]),
+        'postun': cvt(hdr[rpm.RPMTAG_POSTUN]),
+        'icon': hdr[rpm.RPMTAG_ICON],
+        'archivesize': rpm.RPMTAG_ARCHIVESIZE,
+        'rpmversion': cvt(hdr[rpm.RPMTAG_RPMVERSION]),
+        'preinprog': cvt(hdr[rpm.RPMTAG_PREINPROG]),
+        'postinprog': cvt(hdr[rpm.RPMTAG_POSTINPROG]),
+        'preunprog': cvt(hdr[rpm.RPMTAG_PREUNPROG]),
+        'postunprog': cvt(hdr[rpm.RPMTAG_POSTUNPROG]),
+        'buildarchs': cvt(hdr[rpm.RPMTAG_BUILDARCHS]),
+        'verifyscript': cvt(hdr[rpm.RPMTAG_VERIFYSCRIPT]),
+        'verifyscriptprog': cvt(hdr[rpm.RPMTAG_VERIFYSCRIPTPROG]),
+        'cookie': cvt(hdr[rpm.RPMTAG_COOKIE]),
+        'prefixes': cvt(hdr[rpm.RPMTAG_PREFIXES]),
+        'instprefixes': cvt(hdr[rpm.RPMTAG_INSTPREFIXES]),
+        'sourcepackage': bool(rpm.RPMTAG_SOURCEPACKAGE),
+        'optflags': cvt(hdr[rpm.RPMTAG_OPTFLAGS]),
+        'disturl': cvt(hdr[rpm.RPMTAG_DISTURL]),
+        'payloadformat': cvt(hdr[rpm.RPMTAG_PAYLOADFORMAT]),
+        'payloadcompressor': cvt(hdr[rpm.RPMTAG_PAYLOADCOMPRESSOR]),
+        'payloadflags': cvt(hdr[rpm.RPMTAG_PAYLOADFLAGS]),
+        'platform': cvt(hdr[rpm.RPMTAG_PLATFORM]),
+        'sourcepkgid': hdr[rpm.RPMTAG_SOURCEPKGID],
+        'disttag': cvt(hdr[rpm.RPMTAG_DISTTAG]),
+    }
+    return map_package
+
+
+def get_file_map(hdr):
+    map_files = {
+        'filename': cvt(hdr[rpm.RPMTAG_FILENAMES]),
+        'filesize': cvt(hdr[rpm.RPMTAG_FILESIZES]),
+        'filemode': cvt(hdr[rpm.RPMTAG_FILEMODES]),
+        'filerdev': cvt(hdr[rpm.RPMTAG_FILERDEVS]),
+        'filemtime': cvt_ts(hdr[rpm.RPMTAG_FILEMTIMES]),
+        'filemd5': cvt(hdr[rpm.RPMTAG_FILEMD5S]),
+        'filelinkto': cvt(hdr[rpm.RPMTAG_FILELINKTOS]),
+        'fileflag': cvt(hdr[rpm.RPMTAG_FILEFLAGS]),
+        'fileusername': cvt(hdr[rpm.RPMTAG_FILEUSERNAME]),
+        'filegroupname': cvt(hdr[rpm.RPMTAG_FILEGROUPNAME]),
+        'fileverifyflag': cvt(hdr[rpm.RPMTAG_FILEVERIFYFLAGS]),
+        'filedevice': cvt(hdr[rpm.RPMTAG_FILEDEVICES]),
+        'fileinode': cvt(hdr[rpm.RPMTAG_FILEINODES]),
+        'filelang': cvt(hdr[rpm.RPMTAG_FILELANGS]),
+        'fileclass': cvt(hdr[rpm.RPMTAG_FILECLASS]),
+        'dirindex': cvt(hdr[rpm.RPMTAG_DIRINDEXES]),
+        'basename': cvt(hdr[rpm.RPMTAG_BASENAMES]),
+    }
+    return map_files
+
+
+def get_require_map(hdr):
+    map_require = {
+        'name': cvt(hdr[rpm.RPMTAG_REQUIRENAME]),
+        'version': cvt(hdr[rpm.RPMTAG_REQUIREVERSION]),
+        'flag': hdr[rpm.RPMTAG_REQUIREFLAGS],
+    }
+    return map_require
+
+
+def get_conflict_map(hdr):
+    map_conflict = {
+        'name': cvt(hdr[rpm.RPMTAG_CONFLICTNAME]),
+        'version': cvt(hdr[rpm.RPMTAG_CONFLICTVERSION]),
+        'flag': hdr[rpm.RPMTAG_CONFLICTFLAGS],
+    }
+    return map_conflict
+
+
+def get_obsolete_map(hdr):
+    map_obsolete = {
+        'name': cvt(hdr[rpm.RPMTAG_OBSOLETENAME]),
+        'version': cvt(hdr[rpm.RPMTAG_OBSOLETEVERSION]),
+        'flag': hdr[rpm.RPMTAG_OBSOLETEFLAGS],
+    }
+    return map_obsolete
+
+
+def get_provide_map(hdr):
+    map_provide = {
+        'name': cvt(hdr[rpm.RPMTAG_PROVIDENAME]),
+        'version': cvt(hdr[rpm.RPMTAG_PROVIDEVERSION]),
+        'flag': hdr[rpm.RPMTAG_PROVIDEFLAGS],
+    }
+    return map_provide
