56db0c9d2a4055011475cb0b4262904309b922fb
Danil Shein <dshein@altlinux.org>
2021-04-15 14:22:57 +0300

    task.py: complete task structure loader
    - refactor TaskFromFS class
    - add Task structure loader

--- task.py
+++ task.py
@@ -8,14 +8,17 @@ import urllib.error
 import urllib.request
 from collections import defaultdict
 from pathlib import Path
-from datetime import datetime
+import datetime
+import itertools
 
 import clickhouse_driver as chd
 import rpm
 
+import json
+
 import extract
 # from extract import get_header, insert_package, init_cache, check_package
-from utils import get_logger, cvt, md5_from_file, mmhash, sha256_from_file, cvt_ts_to_datetime
+from utils import get_logger, cvt, md5_from_file, mmhash, sha256_from_file, cvt_ts_to_datetime, val_from_json_str, log_parser
 
 NAME = 'task'
 
@@ -235,47 +238,48 @@ class TaskFromFS:
 
     def _get_content(self, url, status=False):
         r = None
+        if status:
+            if Path(url).exists():
+                return True
+            else:
+                return False
         try:
             r = Path(url).read_bytes()
         except IsADirectoryError:
-            # return directory files listing
-            if status:
-                return True
-            return [_ for _ in Path(url).iterdir() if _.is_file()]
+            # return directory listing
+            return [_ for _ in Path(url).iterdir()]
         except FileNotFoundError as e:
             log.debug(f"{e} - {url}")
-            if status:
-                return False
             return None
         except Exception as e:
             log.error(f"{e} - {url}")
             return None
-        if r is not None:
-            if status:
-                return True
-            return r
-
-    def get(self, method, status=False):
-        p = Path.joinpath(self.url, method)
-        r = self._get_content(p, status)
-        return cvt(r)
-
-    def get_bytes(self, method, status=False):
-        p = Path.joinpath(self.url, method)
-        r = self._get_content(p, status)
         return r
 
-    def get_file_mtime(self, method):
-        p = Path.joinpath(self.url, method)
+    def get(self, path):
+        p = Path.joinpath(self.url, path)
+        r = self._get_content(p, status=False)
+        return cvt(r)
+
+    def check(self):
+        return self._get_content(self.url, status=True)
+
+    def check_file(self, path):
+        p = Path.joinpath(self.url, path)
+        return self._get_content(p, status=True)
+
+    def get_bytes(self, path):
+        p = Path.joinpath(self.url, path)
+        r = self._get_content(p, status=False)
+        return r
+
+    def get_file_mtime(self, path):
+        p = Path.joinpath(self.url, path)
         try:
             mtime = p.stat().st_mtime
         except FileNotFoundError:
             return None
-        # return datetime.utcfromtimestamp(mtime).strftime('%Y-%m-%d %H:%M:%S')
-        return cvt_ts_to_datetime(mtime, True)
-
-    def check(self):
-        return self._get_content(self.url, status=True)
+        return cvt_ts_to_datetime(mtime, use_local_tz=False)
 
     def get_header(self, path):
         return extract.get_header(self.ts, str(Path.joinpath(self.url, path)))
@@ -283,6 +287,297 @@ class TaskFromFS:
     def get_file_path(self, path):
         return Path.joinpath(self.url, path)
 
+    def file_exists_and_not_empty(self, path):
+        p = Path.joinpath(self.url, path)
+        if p.is_file() and p.stat().st_size > 0:
+            return True
+        else:
+            return False
+
+    def get_symlink_target(self, path, name_only=False):
+        symlink = Path.joinpath(self.url, path)
+        if symlink.is_symlink():
+            if name_only:
+                return str(symlink.resolve().name)
+            else:
+                return str(symlink.resolve())
+        else:
+            return None
+
+    def parse_approval_file(self, path):
+        p = Path.joinpath(self.url, path)
+        r = self._get_content(p, status=False)
+        n = d = m = None
+        if r:
+            r = cvt(r)
+            try:
+                d, *m = [_ for _ in r.split('\n') if len(_) >0]
+                d, n = [_.strip() for _ in d.split('::') if len(_) >0]
+                n = n.split(' ')[-1]
+                d = datetime.datetime.strptime(d, '%Y-%b-%d %H:%M:%S')
+                m = '\n'.join((_ for _ in m))
+                return (n, d, m)
+            except Exception as e:
+                log.error(f"File parsing failed with error {e} for '{path}' contains '{r}'")
+        return None
+
+
+def init_task_structure_from_task(girar):
+    """Loads all available contents from task to dictionary
+
+    Args:
+        girar (class): Girar class instance initialized with exact task
+
+    Returns:
+        dict: parsed task structure with contents
+    """
+    task = {
+        'tasks': [],
+        'task_state': {},
+        'task_approvals': [],
+        'task_iterations': [],
+        'arepo': [],
+        'logs': []
+    }
+    # parse '/task' and '/info.json' for 'TaskStates'
+    if girar.check_file('task/state'):
+        task['task_state']['task_changed'] = girar.get_file_mtime('task/state')
+    else:
+        # skip tasks with uncertain state for God sake
+        return task
+    task['task_state']['task_id'] = int(girar.get_file_path('').name)
+    t = girar.get('task/state')
+    task['task_state']['task_state'] = t.strip() if t else ''
+    t = girar.get('task/run')
+    task['task_state']['task_runby'] = t.strip() if t else ''
+    t = girar.get('task/depends')
+    task['task_state']['task_depends'] = [int(_) for _ in t.split('\n') if len(_) > 0] if t else []
+    t = girar.get('task/try')
+    task['task_state']['task_try'] = int(t.strip()) if t else 0
+    task['task_state']['task_testonly'] = 1 if girar.check_file('task/test-only') else 0
+    task['task_state']['task_failearly'] = 1 if girar.check_file('task/fail-early') else 0
+    t  = val_from_json_str(girar.get('info.json'), 'shared')
+    task['task_state']['task_shared'] = 1 if t else 0
+    t = girar.get('task/message')
+    task['task_state']['task_message'] = t.strip() if t else ''
+    t = girar.get('task/version')
+    task['task_state']['task_version'] = t.strip() if t else ''
+    t = girar.get_symlink_target('build/repo/prev', name_only=True)
+    task['task_state']['task_prev'] = int(t.strip()) if t else 0
+    # parse '/acl' for 'TaskApprovals'
+    # 0 - iterate through 'acl/approved'
+    for subtask in (_.name for _ in girar.get('acl/approved') if _.is_dir()):
+        subtask_dir = '/'.join(('acl/approved', subtask))
+        for approver in (_.name for _ in girar.get(subtask_dir) if _.is_file()):
+            t = girar.parse_approval_file('/'.join((subtask_dir, approver)))
+            if t:
+                approval = {
+                    'task_id': task['task_state']['task_id'],
+                    'subtask_id': int(subtask),
+                    'tapp_type': 'approve',
+                    # 'tapp_revoked': None,
+                    'tapp_name': t[0],
+                    'tapp_date': t[1],
+                    'tapp_message': t[2]
+                    }
+                task['task_approvals'].append(approval)
+    # 1 - iterate through 'acl/dsiapproved'
+    for subtask in (_.name for _ in girar.get_file_path('acl/disapproved').glob('[0-7]*') if _.is_dir()):
+        subtask_dir = '/'.join(('acl/disapproved', subtask))
+        for approver in (_.name for _ in girar.get(subtask_dir) if _.is_file()):
+            t = girar.parse_approval_file('/'.join((subtask_dir, approver)))
+            if t:
+                approval = {
+                    'task_id': task['task_state']['task_id'],
+                    'subtask_id': int(subtask),
+                    'tapp_type': 'disapprove',
+                    # 'tapp_revoked': None,
+                    'tapp_name': t[0],
+                    'tapp_date': t[1],
+                    'tapp_message': t[2]
+                    }
+                task['task_approvals'].append(approval)
+    # parse '/gears' for 'Tasks'
+    for subtask in (_.name for _ in girar.get_file_path('gears').glob('[0-7]*') if _.is_dir()):
+        subtask_dir = '/'.join(('gears', subtask))
+        files = set((_.name for _ in girar.get(subtask_dir)))
+        sid = girar.get('/'.join((subtask_dir, 'sid')))
+
+        subtask_dict ={
+                'task_id': task['task_state']['task_id'],
+                'subtask_id': int(subtask),
+                'task_repo': girar.get('task/repo').strip(),
+                'task_owner': girar.get('task/owner').strip(),
+                'subtask_changed': None,
+                'subtask_userid': girar.get('/'.join((subtask_dir, 'userid'))).strip(),
+                'subtask_sid': sid.split(':')[1].strip() if sid else '',
+                'subtask_dir': None,
+                'subtask_package': None,
+                'subtask_type': sid.split(':')[0]  if sid else '',
+                'subtask_tag_author': None,
+                'subtask_tag_id': None,
+                'subtask_tag_name': None,
+                'subtask_srpm': None,
+                'subtask_srpm_name': None,
+                'subtask_srpm_evr': None
+        }
+
+        if girar.check_file('/'.join((subtask_dir, 'userid'))):
+            subtask_dict['subtask_changed'] = girar.get_file_mtime('/'.join((subtask_dir, 'userid')))
+        else:
+            subtask_dict['subtask_changed'] = girar.get_file_mtime(subtask_dir)
+
+        if 'dir' not in files and 'srpm' not in files and 'package' not in files:
+            # deleted subtask
+            subtask_dict['subtask_deleted'] = 1
+        else:
+            subtask_dict['subtask_deleted'] = 0
+            # logic from girar-task-run check_copy_del()
+            if girar.file_exists_and_not_empty('/'.join((subtask_dir, 'package'))) \
+                and not girar.file_exists_and_not_empty('/'.join((subtask_dir, 'dir'))):
+                if girar.file_exists_and_not_empty('/'.join((subtask_dir, 'copy_repo'))):
+                    t = girar.get('/'.join((subtask_dir, 'copy_repo')))
+                    subtask_dict['subtask_type'] = f"copy from {t.strip()}"
+                else:
+                    subtask_dict['subtask_type'] = 'delete'
+
+            if girar.check_file('/'.join((subtask_dir, 'rebuild'))):
+                t = girar.get('/'.join((subtask_dir, 'rebuild')))
+                subtask_dict['subtask_type'] = f"rebuild from {t.strip()}"
+
+            t = girar.get('/'.join((subtask_dir, 'dir')))
+            subtask_dict['subtask_dir'] = t.strip() if t else None
+            t = girar.get('/'.join((subtask_dir, 'package')))
+            subtask_dict['subtask_package'] = t.strip() if t else None
+            t = girar.get('/'.join((subtask_dir, 'tag_author')))
+            subtask_dict['subtask_tag_author'] = t.strip() if t else None
+            t = girar.get('/'.join((subtask_dir, 'tag_id')))
+            subtask_dict['subtask_tag_id'] = t.strip() if t else None
+            t = girar.get('/'.join((subtask_dir, 'tag_name')))
+            subtask_dict['subtask_tag_name'] = t.strip() if t else None
+            t = girar.get('/'.join((subtask_dir, 'srpm')))
+            subtask_dict['subtask_srpm'] = t.strip() if t else None
+            t = girar.get('/'.join((subtask_dir, 'nevr')))
+            if t:
+                subtask_dict['subtask_srpm_name'] = t.split('\t')[0].strip()
+                subtask_dict['subtask_srpm_evr'] = t.split('\t')[1].strip()
+        task['tasks'].append(subtask_dict)
+    # parse '/build' for 'TaskIterations'
+    # 0 - get src and packages from plan
+    src_pkgs = {}
+    bin_pkgs = defaultdict(lambda: defaultdict(list))
+    t = girar.get('plan/add-src')
+    if t:
+        for *_, pkg, n in (_.split('\t') for _ in t.split('\n') if len(_) > 0):
+            src_pkgs[n] = pkg
+    t = girar.get('plan/add-bin')
+    if t:
+        for _, _, arch, _, pkg, n, *_ in (_.split('\t') for _ in t.split('\n') if len(_) > 0):
+            bin_pkgs[n][arch].append(pkg)
+    # 1 - get contents from /build/%subtask_id%/%arch%
+    for subtask in (_.name for _ in girar.get_file_path('build').glob('[0-7]*') if _.is_dir()):
+        subtask_dir = '/'.join(('build', subtask))
+        archs = set((_.name for _ in girar.get(subtask_dir) if _.is_dir()))
+        for arch in archs:
+            arch_dir = '/'.join((subtask_dir, arch))
+            build_dict = {
+                'task_id': task['task_state']['task_id'],
+                'subtask_id': int(subtask),
+                'subtask_arch': arch,
+                'titer_ts': None,
+                'titer_status': None,
+                'task_try': None,
+                'task_iter': None,
+                'titer_srpm': None, # 'titer_srcrpm_hash'
+                'titer_rpms': [],   # 'titer_pkgs_hash'
+                'titer_chroot_base': [],
+                'titer_chroot_br': []
+            }
+            if girar.check_file('/'.join((arch_dir, 'status'))):
+                t = girar.get_file_mtime('/'.join((arch_dir, 'status')))
+                tt = girar.get('/'.join((arch_dir, 'status')))
+                build_dict['titer_status'] = tt.strip() if tt else 'failed'
+            else:
+                t = girar.get_file_mtime(arch_dir)
+                build_dict['titer_status'] = 'failed'
+            build_dict['titer_ts'] = t if t else None
+            t = girar.get('task/try')
+            build_dict['task_try'] = int(t.strip()) if t else 0
+            t = girar.get('task/iter')
+            build_dict['task_iter'] = int(t.strip()) if t else 0
+            # read chroots
+            t = girar.get('/'.join((arch_dir, 'chroot_base')))
+            if t:
+                for pkg in (_.split('\t')[-1].strip() for _ in t.split('\n') if len(_) > 0):
+                    build_dict['titer_chroot_base'].append(mmhash(bytes.fromhex(pkg)))
+            t = girar.get('/'.join((arch_dir, 'chroot_BR')))
+            if t:
+                for pkg in (_.split('\t')[-1].strip() for _ in t.split('\n') if len(_) > 0):
+                    build_dict['titer_chroot_br'].append(mmhash(bytes.fromhex(pkg)))
+            # get src and bin packages
+            t = girar.get('/'.join((arch_dir, 'srpm')))
+            if t and len(t) > 0:
+                build_dict['titer_status'] = 'built'
+                src_pkgs[subtask] = '/'.join((arch_dir, t[0].name))
+            if subtask in src_pkgs:
+                build_dict['titer_srpm'] = src_pkgs[subtask]
+
+            t = girar.get('/'.join((arch_dir, 'rpms')))
+            if t and len(t) > 0:
+                build_dict['titer_status'] = 'built'
+                bin_pkgs[subtask][arch] = []
+                for brpm in t:
+                    bin_pkgs[subtask][arch].append('/'.join((arch_dir, brpm.name)))
+
+            if subtask in bin_pkgs and arch in bin_pkgs[subtask]:
+                build_dict['titer_rpms'] = [_ for _ in bin_pkgs[subtask][arch]]
+            task['task_iterations'].append(build_dict)
+            # save build logs
+            for log_file in ('log', 'srpm.log'):
+                if girar.file_exists_and_not_empty('/'.join((arch_dir, log_file))):
+                    log_hash = (''
+                        + str(build_dict['task_id'])
+                        + str(build_dict['subtask_id'])
+                        + str(build_dict['task_try'])
+                        + str(build_dict['task_iter'])
+                        + build_dict['subtask_arch']
+                    )
+                    if log_file == 'log':
+                        log_hash = 'build' + log_hash
+                        task['logs'].append((
+                            'build',
+                            '/'.join((arch_dir, log_file)),
+                            mmhash(log_hash),
+                            log_hash
+                        ))
+                    else:
+                        log_hash = 'srpm'  + log_hash
+                        task['logs'].append((
+                            'srpm',
+                            '/'.join((arch_dir, log_file)),
+                            mmhash(log_hash),
+                            log_hash
+                        ))
+    # parse '/arepo' for packages
+    t = girar.get('arepo/x86_64-i586/rpms')
+    for pkg in (_.name for _ in t if t and _.suffix == '.rpm'):
+            task['arepo'].append(f"arepo/x86_64-i586/rpms/{pkg}")
+    # parse '/logs' for event logs
+    for log_file in (_.name for _ in girar.get_file_path('logs').glob('events.*.log')):
+        log_hash = (
+            'events'
+            + str(task['task_state']['task_id'])
+            + log_file.split('.')[1]
+            + log_file.split('.')[2]
+        )
+        task['logs'].append((
+            'events',
+            '/'.join(('logs', log_file)),
+            mmhash(log_hash),
+            log_hash
+        ))
+
+    return task
 
 def get_args():
     parser = argparse.ArgumentParser()
@@ -318,8 +613,15 @@ def load(args, conn):
     # girar = Girar(args.url)
     girar = TaskFromFS(args.url)
     if girar.check():
-        task = Task(conn, girar)
-        task.save()
+        # task = Task(conn, girar)
+        # task.save()
+        t = init_task_structure_from_task(girar)
+        Path.joinpath(
+            Path.cwd(),
+            f"{str(t['task_state']['task_id'])}_dump.json"
+        ).write_text(
+            json.dumps(t, indent=4, sort_keys=True, default=str)
+        )
     else:
         raise ValueError('task not found: {0}'.format(args.url))
 
