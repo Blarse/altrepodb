decf75f29d68818653538eb9dc2703e8ccbbffff
Danil Shein <dshein@altlinux.org>
2021-10-21 11:15:15 +0300

    task: fix passing package path parameter
    - passing absolute package path instead of relative one

--- task.py
+++ task.py
@@ -251,7 +251,7 @@ class TaskIterationLoaderWorker(RaisingTread):
             kw["pkg_srcrpm_hash"] = srpm_hash
 
         if self.force or not extract.check_package_in_cache(self.cache, hashes["mmh"]):
-            extract.insert_package(self.conn, hdr, pkg, **kw)
+            extract.insert_package(self.conn, hdr, self.girar.get_file_path(pkg), **kw)
             extract.insert_pkg_hash_single(self.conn, hashes)
             self.cache.add(hashes["mmh"])
             self.count += 1
@@ -465,7 +465,7 @@ class PackageLoaderWorker(RaisingTread):
             kw["pkg_srcrpm_hash"] = srpm_hash
 
         if self.force or not extract.check_package_in_cache(self.cache, hashes["mmh"]):
-            extract.insert_package(self.conn, hdr, pkg, **kw)
+            extract.insert_package(self.conn, hdr, self.girar.get_file_path(pkg), **kw)
             extract.insert_pkg_hash_single(self.conn, hashes)
             self.cache.add(hashes["mmh"])
             self.count += 1
