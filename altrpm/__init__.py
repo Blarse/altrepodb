6cd3c62adefcbe3c6d1c2c7225c3fcdeb1970ac5
Danil Shein <dshein@altlinux.org>
2021-12-02 17:20:17 +0300

    add GPLv3 license

--- altrpm/__init__.py
+++ altrpm/__init__.py
@@ -1,3 +1,18 @@
+# This file is part of the altrpm distribution (http://git.altlinux.org/people/dshein/public/altrpm.git).
+# Copyright (c) 2021 BaseALT Ltd
+# 
+# This program is free software: you can redistribute it and/or modify  
+# it under the terms of the GNU General Public License as published by  
+# the Free Software Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY; without even the implied warranty of 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License 
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 from .rpm import RPMHeaders, RPMCpio
 from .rpm import parse_headers_list, parse_xz_headers_list
 from .rpmtag import rpmh as rpm
