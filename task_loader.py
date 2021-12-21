b9c20a3ac95b5f815b0c7f5d2be44599ba01a713
Danil Shein <dshein@altlinux.org>
2021-12-21 15:33:19 +0300

    task_loader: use TaskProcessor class
    - use TaskProcessor class from altrepodb/task

--- task_loader.py
+++ task_loader.py
@@ -1,32 +1,26 @@
 # This file is part of the ALTRepo Uploader distribution (http://git.altlinux.org/people/dshein/public/altrepodb.git).
 # Copyright (c) 2021 BaseALT Ltd
-# 
-# This program is free software: you can redistribute it and/or modify  
-# it under the terms of the GNU General Public License as published by  
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, version 3.
 #
-# This program is distributed in the hope that it will be useful, but 
-# WITHOUT ANY WARRANTY; without even the implied warranty of 
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 # General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License 
+# You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 import os
 import sys
-import json
-import time
-import logging
 import argparse
-import datetime
 import configparser
-from pathlib import Path
 
 from altrepodb.utils import get_logger
-from altrepodb.task import TaskFromFilesystem, TaskLoadHandler, init_task_structure_from_task
+from altrepodb.task import TaskProcessor
 from altrepodb.base import DatabaseConfig, TaskProcessorConfig
-from altrepodb.database import DatabaseClient
 
 
 NAME = "task"
@@ -87,83 +81,39 @@ def get_args():
     return args
 
 
-def load(args, conn, logger):
-    girar = TaskFromFilesystem(args.url, logger)
-    if girar.check():
-        ts = time.time()
-        logger.info(f"reading task structure for {args.url}")
-        task_struct = init_task_structure_from_task(girar, logger)
-        logger.info(f"task structure loaded in {(time.time() - ts):.3f} seconds")
-        if args.dumpjson:
-            p = Path.joinpath(Path.cwd(), "JSON")
-            p.mkdir(exist_ok=True)
-            Path.joinpath(
-                p,
-                f"dump-{str(task_struct['task_state']['task_id'])}-{datetime.date.today().strftime('%Y-%m-%d')}.json",
-            ).write_text(json.dumps(task_struct, indent=2, sort_keys=True, default=str))
-        tp_config = TaskProcessorConfig(
-            id=task_struct['task_state']['task_id'],
-            path=args.url,
-            logger=logger,
-            debug=args.debug,
-            flush=args.flush_buffers,
-            force=args.force,
-            workers=args.workers,
-            dumpjson=args.dumpjson,
-            dbconfig=DatabaseConfig(
-                host=args.host,
-                port=args.port,
-                name=args.dbname,
-                user=args.user,
-                password=args.password
-            )
-        )
-        task = TaskLoadHandler(conn, girar, logger, task_struct, tp_config)
-        logger.info(
-            f"loading task {task_struct['task_state']['task_id']} to database {args.dbname}"
-        )
-        task.save()
-        if args.flush_buffers:
-            logger.info("Flushing buffer tables")
-            task.flush()
-        # update Depends table
-        task.update_depends()
-        ts = time.time() - ts
-        logger.info(
-            f"task {task_struct['task_state']['task_id']} loaded in {ts:.3f} seconds"
-        )
-    else:
-        raise ValueError("task not found: {0}".format(args.url))
-
-
 def main():
     assert sys.version_info >= (3, 7), "Pyhton version 3.7 or newer is required!"
     args = get_args()
     if args.url.endswith("/"):
         args.url = args.url[:-1]
-    logger = get_logger(NAME, tag=(args.url.split("/")[-1]))
+    tag = args.url.split("/")[-1]
+    logger = get_logger(NAME, tag=tag)
     if args.debug:
-        logger.setLevel(logging.DEBUG)
+        logger.setLevel("DEBUG")
     logger.info(f"run with args: {args}")
-    conn = None
+    tp_config = TaskProcessorConfig(
+        id=int(tag),
+        path=args.url,
+        logger=logger,
+        debug=args.debug,
+        flush=args.flush_buffers,
+        force=args.force,
+        workers=args.workers,
+        dumpjson=args.dumpjson,
+        dbconfig=DatabaseConfig(
+            host=args.host,
+            port=args.port,
+            name=args.dbname,
+            user=args.user,
+            password=args.password,
+        ),
+    )
     try:
-        conn = DatabaseClient(
-            config=DatabaseConfig(
-                host=args.host,
-                port=args.port,
-                name=args.dbname,
-                user=args.user,
-                password=args.password
-            ),
-            logger=logger
-        )
-        load(args, conn, logger)
+        tp = TaskProcessor(config=tp_config, logger=logger)
+        tp.run()
     except Exception as error:
         logger.error(str(error), exc_info=True)
         sys.exit(1)
-    finally:
-        if conn is not None:
-            conn.disconnect()
 
 
 if __name__ == "__main__":
