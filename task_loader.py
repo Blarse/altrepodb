20226ab48b5465ec1ccd8a3dc4ded8d60729c23e
Danil Shein <dshein@altlinux.org>
2022-02-18 15:02:42 +0300

    task_loader: add support for logging options from config file

--- task_loader.py
+++ task_loader.py
@@ -19,6 +19,7 @@ import argparse
 import configparser
 
 from altrepodb.logger import get_logger
+from altrepodb.utils import get_logging_options
 from altrepodb.task import TaskProcessor
 from altrepodb.base import DatabaseConfig, TaskProcessorConfig
 
@@ -78,6 +79,7 @@ def get_args():
             except ValueError:
                 workers = 0
             args.workers = max(workers, args.workers)
+            get_logging_options(args, section_db)
     else:
         args.dbname = args.dbname or "default"
         args.host = args.host or "localhost"
@@ -93,7 +95,13 @@ def main():
     if args.url.endswith("/"):
         args.url = args.url[:-1]
     tag = args.url.split("/")[-1]
-    logger = get_logger(NAME, tag=tag)
+    logger = get_logger(
+        NAME,
+        tag=tag,
+        log_to_file=getattr(args, "log_to_file", False),
+        log_to_stderr=getattr(args, "log_to_console", True),
+        log_to_syslog=getattr(args, "log_to_syslog", False),
+    )
     logger.info(f"run with args: {args}")
     try:
         config = TaskProcessorConfig(
