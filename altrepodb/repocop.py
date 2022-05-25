ed34db5bc37034056fc6730d947f84763c8757ee
Danil Shein <dshein@altlinux.org>
2022-05-25 12:31:39 +0300

    repocop: refactor code
    - move core classes to altrepodb/repocop.py
    - rename script file repocop->repocop_loader

--- altrepodb/repocop.py
+++ altrepodb/repocop.py
@@ -13,30 +13,18 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-import sys
 import bz2
 import json
 import requests
-import argparse
-import configparser
 from typing import Any
+from logging import Logger
 from dateutil import tz
 from datetime import datetime
 from dataclasses import dataclass
 from email.utils import parsedate_to_datetime
 from requests.exceptions import RequestException
 
-from altrepodb import (
-    get_config_logger,
-    LoggerLevel,
-    LoggerProtocol,
-    DatabaseClient,
-    DatabaseConfig,
-    DatabaseError,
-)
-
-NAME = "repocop"
-URL_REPOCOP = "http://repocop.altlinux.org/pub/repocop/prometheus3/packages.altlinux-sisyphus.json.bz2"
+from .database import DatabaseClient, DatabaseConfig, DatabaseError
 
 
 class RepocopError(Exception):
@@ -57,7 +45,7 @@ INSERT INTO PackagesRepocop (*) VALUES
 @dataclass
 class RepocopConfig:
     url: str
-    logger: LoggerProtocol
+    logger: Logger
     dbconfig: DatabaseConfig
     timeout: int = 10
 
@@ -107,7 +95,7 @@ class Repocop:
 
     def _save_to_db(self, data: dict[str, Any]) -> None:
         try:
-            res = self.conn.execute(self.sql.insert_repocop, data)
+            _ = self.conn.execute(self.sql.insert_repocop, data)
             self.logger.debug(
                 f"Data loaded to database in {self.conn.last_query_elapsed:.3f} seconds"
             )
@@ -118,7 +106,7 @@ class Repocop:
 
     def run(self) -> None:
         try:
-            self.logger.info(f"Check Repocop for updates")
+            self.logger.info("Check Repocop for updates")
             repocop_date = self._get_header_modified()
             last_db_update_date = self._get_db_modified()
 
@@ -135,74 +123,3 @@ class Repocop:
             raise e
         finally:
             self.conn.disconnect()
-
-
-def get_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-c", "--config", type=str, help="Path to configuration file")
-    parser.add_argument("-d", "--dbname", type=str, help="Database name")
-    parser.add_argument("-s", "--host", type=str, help="Database host")
-    parser.add_argument("-p", "--port", type=str, help="Database password")
-    parser.add_argument("-u", "--user", type=str, help="Database login")
-    parser.add_argument("-P", "--password", type=str, help="Database password")
-    parser.add_argument(
-        "-D", "--debug", action="store_true", help="Set logging level to debug"
-    )
-    args = parser.parse_args()
-
-    if args.config is not None:
-        cfg = configparser.ConfigParser()
-        with open(args.config) as f:
-            cfg.read_file(f)
-        if cfg.has_section("DATABASE"):
-            section_db = cfg["DATABASE"]
-            args.dbname = args.dbname or section_db.get("dbname", "default")
-            args.host = args.host or section_db.get("host", "localhost")
-            args.port = args.port or section_db.get("port", None)
-            args.user = args.user or section_db.get("user", "default")
-            args.password = args.password or section_db.get("password", "")
-    else:
-        args.dbname = args.dbname or "default"
-        args.host = args.host or "localhost"
-        args.port = args.port or None
-        args.user = args.user or "default"
-        args.password = args.password or ""
-
-    return args
-
-
-def main():
-    assert sys.version_info >= (3, 7), "Pyhton version 3.7 or newer is required!"
-    args = get_args()
-    logger = get_config_logger(
-        NAME,
-        tag="load",
-        config=args.config,
-    )
-    if args.debug:
-        logger.setLevel(LoggerLevel.DEBUG)
-    logger.debug(f"Run with args: {args}")
-    try:
-        rp = Repocop(
-            RepocopConfig(
-                url=URL_REPOCOP,
-                logger=logger,
-                dbconfig=DatabaseConfig(
-                    host=args.host,
-                    port=args.port,
-                    name=args.dbname,
-                    user=args.user,
-                    password=args.password,
-                ),
-                timeout=30,
-            )
-        )
-        rp.run()
-
-    except Exception as error:
-        logger.error(f"Error occurred while loading Repocop: {error}", exc_info=True)
-        sys.exit(1)
-
-
-if __name__ == "__main__":
-    main()
