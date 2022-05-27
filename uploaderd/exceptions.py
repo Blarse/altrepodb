a17f17a7458cb5dfc7a70779f30b7fd3283436ee
Danil Shein <dshein@altlinux.org>
2022-05-27 17:30:13 +0300

    fix code style

--- uploaderd/exceptions.py
+++ uploaderd/exceptions.py
@@ -11,6 +11,7 @@ class ServiceFailMessage(ServiceError):
     def __init__(self):
         super().__init__("Service Failed")
 
+
 class ServiceLoadConfigError(ServiceError):
     def __init__(self, message: str = ""):
         super().__init__(f"Service Load Config Error: {message}")
