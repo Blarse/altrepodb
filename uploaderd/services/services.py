582d15b7a0426c89b7e122405c9a2eb299eebffa
Danil Shein <dshein@altlinux.org>
2022-05-27 17:55:28 +0300

    remove test service

--- uploaderd/services/services.py
+++ uploaderd/services/services.py
@@ -1,6 +1,5 @@
 from ..service import ServiceBase
 
-from .test_service import TestService
 from .task_service import TaskLoaderService
 from .bugzilla_service import BugzillaLoaderService
 
@@ -8,5 +7,4 @@ from .bugzilla_service import BugzillaLoaderService
 SERVICES: dict[str, type[ServiceBase]] = {
     "task_loader": TaskLoaderService,
     "bug_loader": BugzillaLoaderService,
-    "test_service": TestService,
 }
